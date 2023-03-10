8d37e7c3ace4903b2c97d56e1c37482b6aaf5221
Pedro Iriano <pdzeus83aw@gmail.com>
2023-03-10 10:02:10 +0700

    Adding Route Group Admin

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+Route::middleware(['auth','role:admin'])->group(function() {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
