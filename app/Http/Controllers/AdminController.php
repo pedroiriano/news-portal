8f83fe5621a2831c25a81e3587007f7e5971deeb
Pedro Iriano <pdzeus83aw@gmail.com>
2023-03-21 11:37:16 +0700

    Edit Space

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -15,11 +15,8 @@ class AdminController extends Controller
     public function AdminLogout(Request $request)
     {
         Auth::guard('web')->logout();
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
-
         return redirect('/');
     }
 
