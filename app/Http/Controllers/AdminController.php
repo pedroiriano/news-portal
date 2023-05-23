3aec38f7894323f71ad4d46ba1697d2667dc9bf0
Pedro Iriano <pdzeus83aw@gmail.com>
2023-05-23 10:45:53 +0700

    Create AdminProfile Function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -25,5 +26,12 @@ class AdminController extends Controller
     {
         $pageTitle = "Login | Prokopim";
         return view('admin.admin-login', compact('pageTitle'));
+    }
+
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin-profile-view', compact('adminData'));
     }
 }
