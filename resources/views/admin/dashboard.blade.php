fae65aed0ecdbcbf806588b4208eebfd4a7e3fdb
Pedro Iriano <pdzeus83aw@gmail.com>
2023-05-23 10:46:45 +0700

    Change Breadcrumbs Link

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,17 +13,17 @@
             <div class="page-title">
                 <div class="row">
                     <div class="col-6">
-                        <h3>Layout Light</h3>
+                        <h3>Dashboard</h3>
                     </div>
                     <div class="col-6">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item">
-                                <a href="index.html">
+                                <a href="{{ route('admin.dashboard') }}">
                                     <i data-feather="home"></i>
                                 </a>
                             </li>
-                            <li class="breadcrumb-item">Color version</li>
-                            <li class="breadcrumb-item active">Layout light</li>
+                            <li class="breadcrumb-item">Dashboard</li>
+                            <li class="breadcrumb-item active">Main</li>
                         </ol>
                     </div>
                 </div>
