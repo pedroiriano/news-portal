cd63a9fb3c4bb074f0a6b564159356b9416d9f04
Pedro Iriano <pdzeus83aw@gmail.com>
2023-05-23 10:45:33 +0700

    Add Profile Route

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,5 @@ Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->middleware(R
 Route::middleware(['auth','role:admin'])->group(function() {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
 });
