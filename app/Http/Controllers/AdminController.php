4946b97f0412a80c8c473e4e1ed5530d1ffeda8f
Pedro Iriano <pdzeus83aw@gmail.com>
2023-06-06 14:35:37 +0700

    Add Admin Profile Update Function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use App\Models\User;
 
 class AdminController extends Controller
@@ -34,5 +36,48 @@ class AdminController extends Controller
         $id = Auth::user()->id;
         $adminData = User::find($id);
         return view('admin.admin-profile-view', compact('pageTitle', 'adminData'));
+    }
+
+    public function AdminProfileUpdate(Request $request, $id)
+    {
+        $this->validate($request, [
+            'image' => 'image|mimes:jpg,jpeg,png,gif,bmp|max:150000',
+        ]);
+
+        $user = auth()->user();
+
+        if($request->hasFile('image')) {
+            $photoNameWithExt = $request->file('image')->getClientOriginalName();
+            $photoName = pathinfo($photoNameWithExt, PATHINFO_FILENAME);
+            $photoName = $request->input('name');
+            $convertPhotoName = Str::lower($photoName);
+            $collectionConvertPhotoName = Str::of($convertPhotoName)->explode(' ');
+            $collectionConvertPhotoName = $collectionConvertPhotoName->implode('-');
+            $photoExt = $request->file('image')->getClientOriginalExtension();
+            $photoNameSaved = $collectionConvertPhotoName.'-'.time().'.'.$photoExt;
+            $photoPath = $request->file('image')->storeAs('public/adminphoto', $photoNameSaved);
+        }
+        else {
+            
+        }
+
+        $data = User::findOrFail($id);
+        $data->name = $request->input('name');
+        $data->username = $request->input('username');
+        $data->email = $request->input('email');
+        $data->phone = $request->input('phone');
+
+        if($request->hasFile('image')) {
+            $oldPhoto = $data->photo;
+            Storage::delete('public/adminphoto/'.$oldPhoto);
+            $data->photo = $photoNameSaved;
+        }
+        else {
+            $oldPhoto = $data->photo;
+            $data->photo = $oldPhoto;
+        }
+        $data->save();
+
+        return redirect()->route('admin.profile')->with('success', 'Profile Changed Successfully');
     }
 }
