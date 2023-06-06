1908089bdc0ab9aeaaee31b777cdaa5e131a22ec
Pedro Iriano <pdzeus83aw@gmail.com>
2023-06-06 14:36:49 +0700

    Add Admin Profile Update Route

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,5 @@ Route::middleware(['auth','role:admin'])->group(function() {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::put('/admin/profile/{admin}', [AdminController::class, 'AdminProfileUpdate'])->name('admin.profile.update');
 });
