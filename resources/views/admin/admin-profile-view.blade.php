6a51dfa0fd2ade1e588fdbd8ca2e24ab718257fd
Pedro Iriano <pdzeus83aw@gmail.com>
2023-06-06 14:36:15 +0700

    Change Admin Photo Link

--- resources/views/admin/admin-profile-view.blade.php
+++ resources/views/admin/admin-profile-view.blade.php
@@ -49,7 +49,7 @@
                             <div class="row mb-2">
                                 <div class="profile-title">
                                     <div class="media">
-                                        <img class="img-70 rounded-circle" alt="" src="{{ (empty($adminData->photo)) ? asset('backend/assets/images/user/7.jpg') : asset('backend/assets/images/user/admin/'.$adminData->photo) }}">
+                                        <img class="img-70 rounded-circle" alt="Profile Photo" src="{{ (empty($adminData->photo)) ? asset('backend/assets/images/user/7.jpg') : asset('storage/adminphoto/'.$adminData->photo) }}">
                                         <div class="media-body">
                                             <h5 class="mb-1">{{ $adminData->name }}</h5>
                                             <p>{{ $adminData->role }}</p>
@@ -73,8 +73,9 @@
                     </div>
                 </div>
                 <div class="col-xl-8">
-                    <form class="card" id="adminProfileForm" method="POST" action="{{ empty($adminData) ? route('admin.profile') : route('admin.profile', $adminData->id) }}" enctype="multipart/form-data">
+                    <form class="card" id="adminProfileForm" method="POST" action="{{ empty($adminData) ? route('admin.profile.store') : route('admin.profile.update', $adminData->id) }}" enctype="multipart/form-data">
                         @csrf
+                        @method('PUT')
                         <div class="card-header">
                             <h4 class="card-title mb-0">Edit Profile</h4>
                             <div class="card-header-right">
@@ -93,25 +94,25 @@
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="userName" autofocus>Username</label>
-                                        <input class="form-control" id="userName" type="text" name="username" placeholder="Username">
+                                        <input class="form-control" id="userName" type="text" name="username" placeholder="Username" value="{{ $adminData->username }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="emailAddress">Email address</label>
-                                        <input class="form-control" id="emailAddress" type="email" name="email" placeholder="Email">
+                                        <input class="form-control" id="emailAddress" type="email" name="email" placeholder="Email" value="{{ $adminData->email }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="fullName">Full Name</label>
-                                        <input class="form-control" id="fullName" type="text" name="name" placeholder="Full Name">
+                                        <input class="form-control" id="fullName" type="text" name="name" placeholder="Full Name" value="{{ $adminData->name }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="phoneNumber">Phone Number</label>
-                                        <input class="form-control" id="phoneNumber" type="number" name="phone" placeholder="Phone Number">
+                                        <input class="form-control" id="phoneNumber" type="number" name="phone" placeholder="Phone Number" value="{{ $adminData->phone }}">
                                     </div>
                                 </div>
                                 <div class="col-md-12">
@@ -124,18 +125,17 @@
                                         </select>
                                     </div>
                                 </div>
-                                {{-- <div class="col-md-12">
+                                <div class="col-md-6">
                                     <div class="mb-3">
-                                        <label class="form-label" for="singleFileUpload">Upload Photo</label>
-                                        <div class="dropzone" id="singleFileUpload" action="/file-upload">
-                                            <input type="hidden" name="images" value="images" id="images">
-                                            <div class="dz-message needsclick">
-                                                <i class="icon-cloud-up"></i>
-                                                <h6>Drop files here or click to upload.</h6>
-                                            </div>
-                                        </div>
+                                        <label class="form-label" for="imageUpload">Upload Photo</label>
+                                        <input class="form-control" id="imageUpload" name="image" type="file">
                                     </div>
-                                </div> --}}
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <img class="img-70 rounded-circle" id="imageShow" alt="Profile Photo" src="{{ (empty($adminData->photo)) ? asset('backend/assets/images/user/7.jpg') : asset('storage/adminphoto/'.$adminData->photo) }}">
+                                    </div>
+                                </div>
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label" for="newPassword">New Password</label>
@@ -161,4 +161,16 @@
     {{-- END::Page Body Container --}}
 </div>
 {{-- END::Page Body --}}
+
+<script>
+    $(document).ready(function() {
+        $('#imageUpload').change(function(e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#imageShow').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 @endsection
